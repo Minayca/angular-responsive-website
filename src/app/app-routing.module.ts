d7ef7898b5af60c888cd6b9470065c9f28e33b30
Minayca <minaycakanberk@gmail.com>
2021-05-10 17:45:21 +0300

    Angular kullanarak responsive web sitesi tasarımı

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { PlanComponent } from './components/plan/plan.component';
+import { ServiceComponent } from './components/service/service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "", component:HomeComponent},
+  {path : "home", component:HomeComponent},
+  {path : "about", component:AboutComponent},
+  {path : "service", component:ServiceComponent},
+  {path : "plan", component:PlanComponent},
+  {path : "contact", component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceComponent } from './components/service/service.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PlanComponent } from './components/plan/plan.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceComponent,
+    HomeComponent,
+    AboutComponent,
+    PlanComponent,
+    ContactComponent,
+    FooterComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
